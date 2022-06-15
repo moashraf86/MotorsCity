268c79f14b0a03fdc36349573cc4a00ee899bf87
Mohamed Ashraf <botoashraf123@gmail.com>
2022-06-16 01:13:33 +0200

    add car-details page

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,8 +107,40 @@ $('aside.filter .filter-head .icon').on('click', function() {
   $('html, body').css('overflow-y', 'scroll')
 })
 
-// slider
-  // Initialise Carousel
+
+let initOffset  = $('#vehicle-nav').offset().top;
+let sectionIds  = ['#overview', '#report'];
+$(window).on('scroll', function() {
+  // add class "active" to nav item when its associated section comes into view
+  $('section').each(function() {
+    if($(window).scrollTop() >= $(this).offset().top - 1) {
+      var bId = `${$(this).attr('id')}` ;
+      $('#vehicle-nav a').removeClass('active');
+      $('#vehicle-nav a[href="' + '#' + bId + '"]').addClass('active');
+    }
+  });
+  // Give vehicle-nav style when it reaches the top of the page
+  let currentOffset  = $('#vehicle-nav').offset().top;
+  if(currentOffset == initOffset) {
+    $('#vehicle-nav').removeClass('active')
+  } else {
+    $('#vehicle-nav').addClass('active')
+  }
+  
+});
+
+// Select between Finance Providers 
+$('.providers li').click( function() {
+  $(this).addClass('active').siblings().removeClass('active');
+  let dataMonthly = $(this).attr('data-monthly');
+  let dataCash = $(this).attr('data-cash');
+  $(this).parent().siblings('.card').find('input#monthly-payment, input#monthly-range').val(dataMonthly)
+  $(this).parent().siblings('.card').find('span#monthly-value').text(dataMonthly);
+  $(this).parent().siblings('.card').find('input#cash-down, input#cash-range').val(dataCash)
+  $(this).parent().siblings('.card').find('span#cash-value').text(dataCash)
+})
+
+
 // Initialise Carousel
 const mainCarousel = new Carousel(document.querySelector("#mainCarousel"), {
   infinite: false,
