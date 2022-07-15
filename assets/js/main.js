d7968cb4c385a4292d7906dc9676e43126f1f076
Mohamed Ashraf <botoashraf123@gmail.com>
2022-07-15 23:35:06 +0200

    Add "Bootstrap-input-slider" to search filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,33 @@
+//Double range slider input
+var doubleSlider  = $('.double-range-input').slider({
+  formatter:function() {
+    let input = $('[data-slider-id='+ $(this).attr('id') +']');
+    var values = input.slider('getValue');
+    input.parents('form').find('input#min-payment').val(values[0])
+    input.parents('form').find('input#max-payment').val(values[1])
+  }
+});
+var double_initial_values = $('.double-range-input').slider('getValue');
+let minPayment = double_initial_values[0]
+let maxPayment = double_initial_values[1]
+//set initial values for Min & Max payment
+$('.filter input#min-payment').val(minPayment)
+$('.filter input#max-payment').val(maxPayment)
 
+
+// Single Range slider Input 
+var singleSlider  = $('.single-range-input').slider({
+  formatter:function() {
+    let input = $('[data-slider-id='+ $(this).attr('id') +']');
+    var value = input.slider('getValue');
+    // var value = $('.single-range-input').slider('getValue');
+    input.parent().find('input[type=number]').val(value)
+    input.parent().find('.range-val').text(value)
+  }
+});
+//set initial values for Min & Max payment
+var single_initial_value = $('.single-range-input').slider('getValue');
+$('.single-range-input').parent().find('input[type=number]').val(single_initial_value)
 
 // Make Driver Seats Slider works when click on nav-tabs
 $('#driver-seat .nav-tabs .nav-link').each(function() {
@@ -53,7 +82,7 @@ $('input[type=number]').each(function() {
   $(this).attr('value', intialVal)
 })
 $('.range-val').each(function() {
-  let intialVal = $(this).parent().siblings('input[type=range]').val();
+  let intialVal = $(this).parent().siblings('input').val();
   $(this).text(intialVal);
 })
 
